f0362110c7c2bfd1d53d5a29b3616fe8b9a9117a
Oleg Bespalov <olegbespalov@gmail.com>
2025-02-27 14:24:09 +0100

    Introduces Reader into k6deps.Source (#69)

--- archive_test.go
+++ archive_test.go
@@ -1,6 +1,7 @@
 package k6deps
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -26,6 +27,26 @@ func Test_analyzeArchive(t *testing.T) {
 
 	expected, err := Analyze(opts)
 
+	require.NoError(t, err)
+
+	require.Equal(t, expected.String(), actual.String())
+}
+
+func Test_analyzeArchive_Reader(t *testing.T) {
+	t.Parallel()
+
+	file, err := os.Open(filepath.Join("testdata", "archive.tar")) //nolint:forbidigo
+	require.NoError(t, err)
+	defer file.Close() //nolint:errcheck
+
+	opts := &Options{
+		Archive: Source{Reader: file},
+	}
+
+	actual, err := Analyze(opts)
+	require.NoError(t, err)
+
+	expected, err := Analyze(opts)
 	require.NoError(t, err)
 
 	require.Equal(t, expected.String(), actual.String())
