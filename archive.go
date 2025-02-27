ab0792d53b1ca2853334f9f912a5a235af26d23a
pablochacin <pablochacin@gmail.com>
2025-02-27 14:11:40 +0100

    Follow links (#72)
     follow links in the archive
    
    ---------
    
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- archive.go
+++ archive.go
@@ -40,7 +40,7 @@ func loadMetadata(dir string, opts *Options) error {
 		opts.Env.Ignore = true
 	}
 
-	contents, err := os.ReadFile(filepath.Join(filepath.Clean(dir), "data"))
+	contents, err := os.ReadFile(opts.Script.Name)
 	if err != nil {
 		return err
 	}
@@ -60,11 +60,11 @@ type archiveMetadata struct {
 	Env      map[string]string `json:"env"`
 }
 
+const maxFileSize = 1024 * 1024 * 10 // 10M
+
 //nolint:forbidigo
 func extractArchive(dir string, input io.Reader) error {
 	reader := tar.NewReader(input)
-
-	const maxFileSize = 1024 * 1024 * 10 // 10M
 
 	for {
 		header, err := reader.Next()
@@ -87,7 +87,7 @@ func extractArchive(dir string, input io.Reader) error {
 			}
 
 		case tar.TypeReg:
-			if ext := filepath.Ext(target); ext == ".csv" || (ext == ".json" && filepath.Base(target) != "metadata.json") {
+			if shouldSkip(target) {
 				continue
 			}
 
@@ -103,6 +103,56 @@ func extractArchive(dir string, input io.Reader) error {
 			if err = file.Close(); err != nil {
 				return err
 			}
+
+		// if it is a link or symlink, we copy the content of the linked file to the target
+		// we assume the linked file was already processed and exists in the directory.
+		case tar.TypeLink, tar.TypeSymlink:
+			if shouldSkip(target) {
+				continue
+			}
+
+			linkedFile := filepath.Join(dir, filepath.Clean(filepath.FromSlash(header.Linkname)))
+			if err := followLink(linkedFile, target); err != nil {
+				return err
+			}
 		}
 	}
+}
+
+// indicates if the file should be skipped during extraction
+// we skip csv files and .json except metadata.json
+func shouldSkip(target string) bool {
+	ext := filepath.Ext(target)
+	return ext == ".csv" || (ext == ".json" && filepath.Base(target) != "metadata.json")
+}
+
+//nolint:forbidigo
+func followLink(linkedFile string, target string) error {
+	source, err := os.Open(filepath.Clean(linkedFile))
+	if err != nil {
+		return err
+	}
+	defer source.Close() //nolint:errcheck
+
+	// we need to get the lined file info to create the target file with the same permissions
+	info, err := source.Stat()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(target, os.O_CREATE|os.O_WRONLY, info.Mode()) //nolint:gosec
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, source)
+	if err != nil {
+		return err
+	}
+
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
