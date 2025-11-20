62541e4ad465398e14f9ca1df897e686ad348a56
pablochacin <pablochacin@gmail.com>
2025-11-20 21:25:10 +0100

    update golangci-lint to v1.64.8 (#158)
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- dependency.go
+++ dependency.go
@@ -22,7 +22,7 @@ const (
 
 //nolint:gochecknoglobals
 var (
-	ErrConstraints = errors.New("constraints error")
+	ErrConstraints = errors.New("constraints error") //nolint:revive
 	ErrDependency  = errors.New("dependency error")
 
 	defaultConstraints, _ = semver.NewConstraint(defaultConstraintsString)
